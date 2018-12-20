60157b51f05e4910b7f6d5817f860e6c7cff0fa0
BT_1N3_10 <BT_1N3_10>
2018-12-20 11:22:50 -0500

    added methods

--- src/DarrenYau/Main.java
+++ src/DarrenYau/Main.java
@@ -27,7 +27,20 @@ public class Main {
     public static int[] rowSums(int[][] arr2D) {
         int[] sumArr = new int[arr2D.length];
         for(int i = 0; i < arr2D.length; i++){
-            su
+            sumArr[i] = arraySum(arr2D[i]);
         }
+        return sumArr;
+    }
+
+    public static boolean isDiverse(int[][] arr2D) {
+        int[] sumArr = rowSums(arr2D);
+        for(int i = 0; i < sumArr.length; i++) {
+            for(int j = i + 1; j < sumArr.length; j++) {
+                if (sumArr[i] == sumArr[i]) {
+                        return false;
+                }
+            }
+        }
+        return true;
     }
 }
